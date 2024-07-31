77ad03f0bdb45e7e31af086095f64de2435dda4f
Colin Smith <colin.smith@wisc.edu>
2024-07-31 15:36:46 -0700

    Define major function signatures

---
+++ main.py
@@ -0,0 +1,37 @@
+
+
+def get_data(dir):
+    """Get data for curation capacity analysis"""
+
+    # Parameterize this function
+    uids = [
+        "cgries",
+        "mobrien",
+        "sgrossmanclarke1",
+        "csmith",
+        "kzollovenecek",
+        "bmcafee",
+        "jlemaire",
+        "skskoglund",
+        "EDI",
+    ]
+    start = "2018-01-01"
+    end = now
+    bin_size = 'week'
+
+    # Get package ID and date published
+    return None
+
+
+def convert_data(data):
+    # Reformat for plotting
+    # Bin to frequency
+
+    return None
+
+def plot_data(data):
+    # Plot
+    # - average, std,
+
+    return None
+
